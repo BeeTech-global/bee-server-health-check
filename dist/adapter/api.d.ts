9fa88f3d868b7063f94cb0231b5a686f9142e5c3
Vinicius Moraes <vinicius.moraes@beetech.global>
2021-01-11 09:52:59 -0300

    Remove dist folder

--- dist/adapter/api.d.ts
+++
@@ -1,8 +0,0 @@
-import { Adapter, AdapterResponse } from '../index';
-export default class ApiAdapter implements Adapter {
-    readonly name: string;
-    readonly host: string;
-    readonly isRequired: boolean;
-    constructor(name: string, host: string, isRequired: boolean);
-    check(): Promise<AdapterResponse>;
-}
--- dist/adapter/knex-db.d.ts
+++
@@ -1,9 +0,0 @@
-import { Adapter, AdapterResponse } from '../index';
-export default class KnexDBAdapter implements Adapter {
-    readonly name: string;
-    readonly host: string;
-    readonly isRequired: boolean;
-    private readonly knexInstance;
-    constructor(name: string, host: string, isRequired: boolean, knexInstance: any);
-    check(): Promise<AdapterResponse>;
-}
